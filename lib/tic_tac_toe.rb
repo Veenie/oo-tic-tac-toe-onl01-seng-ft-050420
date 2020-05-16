9e09309832c0ba653fcb706b19629e5eaef33021
Raveen K Sharma <raveen08@gmail.com>
2020-05-16 20:45:35 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,10 +32,10 @@ class TicTacToe
  end
  
  def position_taken?(index)
-   if @board[index] == "X" || "O"
-     true
-   else
+   if @board[index] == nil
      false
+   else
+     true
   end
  end
 
