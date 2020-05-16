9c40bebcfa5ac0508129fd55da28e3fe555a6b2d
Raveen K Sharma <raveen08@gmail.com>
2020-05-16 21:40:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -53,7 +53,7 @@ class TicTacToe
    puts "Enter 1-9"
    user_input = gets.strip
    @board[index] = input_to_index(user_input)
-   
+   if 
  
  def turn_count
    @board.count{|token| token == "X" || token == "O"}
